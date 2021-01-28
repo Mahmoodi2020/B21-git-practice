8d6da8f8fed49854d30d0eca8a38a6fca820325d
Mahmoodi2020 <khojest@hotmail.com>
2021-01-28 07:08:54 -0700

    added something new to movie

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -3,6 +3,6 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello World, B21");
         System.out.println("B21 are stars");
-
+        // watching more movies
     }
 }
