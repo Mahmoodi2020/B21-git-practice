d1c9ca2e9b8a669bda98da24c0213cf29fb2f680
Mahmoodi2020 <khojest@hotmail.com>
2021-01-28 07:25:07 -0700

    movie 2 will be on the phone in 10 mints

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -5,6 +5,7 @@ public class Hello {
         System.out.println("B21 are stars");
         System.out.println("ready to go back to where ever");
         System.out.println("add something to test");
+        System.out.println("Movie 2 is on the phone 15 mint");
 
     }
 }
