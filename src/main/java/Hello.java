6ac84d8f934c97e157e6ccdfd89564fa97c409f5
Mahmoodi2020 <khojest@hotmail.com>
2021-01-28 07:16:16 -0700

    added something to this branch to make it work

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -4,6 +4,7 @@ public class Hello {
         System.out.println("Hello World b 22" );
         System.out.println("B21 are stars");
         System.out.println("ready to go back to where ever");
+        System.out.println("add something to test");
 
     }
 }
